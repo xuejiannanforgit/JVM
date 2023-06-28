2dd8c9c36e4ce5654651cb6c4490e8767903d26f
xuejiannan <330288681@qq.com>
2023-06-28 16:10:09 +0800

    first commit

---
+++ src/main/java/chapter01/StackStruTest.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class StackStruTest {
+    public static void main(String[] args) {
+//        int i = 2 + 3;
+
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+
+
+
+    }
+}
